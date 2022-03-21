6e3edd8e6f8fa9cf8d0417636e27efa6cf6483b8
CianGibbons <ciangibb@gmail.com>
2022-03-21 00:18:38 +0000

    Added Indication of which day is Today on Home Page

--- lib/components/day_representation.dart
+++ lib/components/day_representation.dart
@@ -1,32 +1,58 @@
 import 'package:flutter/material.dart';
 
 class DayRepresentation extends StatelessWidget {
-  const DayRepresentation(
-      {Key? key,
-      required this.workoutPresent,
-      required this.dayValue,
-      required this.dayReached})
-      : super(key: key);
+  const DayRepresentation({
+    Key? key,
+    required this.workoutPresent,
+    required this.dayValue,
+    required this.dayReached,
+  }) : super(key: key);
   final bool workoutPresent;
   final bool dayReached;
   final int dayValue;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Text(getDayShort()),
-          Icon(
-            dayReached
-                ? (workoutPresent ? Icons.done : Icons.clear)
-                : Icons.more_time,
-            color: dayReached
-                ? (workoutPresent ? Colors.green : Colors.red)
-                : Colors.white60,
-          ),
-        ],
+    bool isToday = false;
+    var d = DateTime.now();
+    var weekDay = d.weekday;
+    if (weekDay == dayValue) {
+      isToday = true;
+    }
+
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(getDayShort()),
+            isToday
+                ? Container(
+                    width: 28,
+                    height: 28,
+                    decoration: BoxDecoration(
+                      color: Colors.green.withOpacity(0.25), // border color
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      dayReached
+                          ? (workoutPresent ? Icons.done : Icons.clear)
+                          : Icons.more_time,
+                      color: dayReached
+                          ? (workoutPresent ? Colors.green : Colors.red)
+                          : Colors.blueGrey,
+                    ),
+                  )
+                : Icon(
+                    dayReached
+                        ? (workoutPresent ? Icons.done : Icons.clear)
+                        : Icons.more_time,
+                    color: dayReached
+                        ? (workoutPresent ? Colors.green : Colors.red)
+                        : Colors.blueGrey,
+                  ),
+          ],
+        ),
       ),
     );
   }
@@ -41,7 +67,7 @@ class DayRepresentation extends StatelessWidget {
         dayShort = "WED";
         break;
       case 4:
-        dayShort = "THUR";
+        dayShort = "THU";
         break;
       case 5:
         dayShort = "FRI";
